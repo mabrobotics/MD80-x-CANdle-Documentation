9cd3e5313c17042c83570312389dca49d1c61ee1
MilkoRoszko <meimek13@gmail.com>
2026-09-21 11:40:54 +0200

    fix: patched paths to .ini generate file

--- generate_ini_file_list.py
+++ generate_ini_file_list.py
@@ -2,7 +2,11 @@ import os
 import glob
 import re
 
-folder_to_scan = "./_static/firmware/"
+folders_to_scan = [
+    "./_static/firmware/candle",
+    "./_static/firmware/md/legacy",
+    "./_static/firmware/md"
+]
 output_file = "./_static/firmware/api_download.ini"
 
 raw_base = "https://mabrobotics.github.io/MD80-x-CANdle-Documentation/_static/firmware/"
@@ -15,63 +19,78 @@ DEVICE_MAP = {
     "flasher": "MD_FLASHER" 
 }
 
+# Lista rozszerzeń, które chcemy usuwać z nazwy sekcji
+KNOWN_EXTENSIONS = ['.exe', '.bin', '.hex', '.elf', '.zip', '.tar.gz', '.appimage']
+
 def generate_ini_list():
-    search_pattern = os.path.join(folder_to_scan, "*")
     grouped_assets = {}
-    
     output_filename = os.path.basename(output_file)
 
     arch_pattern = re.compile(r'_(x86_64|arm64|armhf|aarch64|amd64)', re.IGNORECASE)
 
-    for f in glob.glob(search_pattern):
-        if os.path.isfile(f):
-            filename = os.path.basename(f)
+    for folder in folders_to_scan:
+        search_pattern = os.path.join(folder, "*")
+        
+        for f in glob.glob(search_pattern):
+            if os.path.isfile(f):
+                filename = os.path.basename(f)
 
-            if filename == output_filename or filename.endswith(".json"):
-                continue
+                if filename == output_filename or filename.endswith(".json"):
+                    continue
 
-            name_only, extension = os.path.splitext(filename)
-            filename_lower = filename.lower()
-            
-            tag_name = "UNKNOWN" 
-            for search_key, target_device in DEVICE_MAP.items():
-                if search_key in filename_lower:
-                    tag_name = target_device
-                    break
+                rel_path = os.path.relpath(f, "./_static/firmware/")
+                rel_path = rel_path.replace("\\", "/") 
+                
+                filename_lower = filename.lower()
 
-            arch_match = arch_pattern.search(name_only)
-            if arch_match:
-                arch = arch_match.group(1).lower()
-                section_name = name_only[:arch_match.start()]
-            else:
-                arch = "default"
-                section_name = name_only
+                # Bezpieczne usuwanie rozszerzenia
+                name_only = filename
+                for ext in KNOWN_EXTENSIONS:
+                    if filename_lower.endswith(ext):
+                        name_only = filename[:-len(ext)]
+                        break
+                
+                tag_name = "UNKNOWN" 
+                for search_key, target_device in DEVICE_MAP.items():
+                    if search_key in filename_lower:
+                        tag_name = target_device
+                        break
 
-            if section_name not in grouped_assets:
-                grouped_assets[section_name] = {
-                    "type": tag_name,
-                    "files": {}
+                arch_match = arch_pattern.search(name_only)
+                if arch_match:
+                    arch = arch_match.group(1).lower()
+                    section_name = name_only[:arch_match.start()]
+                else:
+                    arch = "default"
+                    section_name = name_only
+
+                if section_name not in grouped_assets:
+                    grouped_assets[section_name] = {
+                        "type": tag_name,
+                        "files": {}
+                    }
+                
+                grouped_assets[section_name]["files"][arch] = {
+                    "filename": filename,
+                    "rel_path": rel_path
                 }
-            
-            grouped_assets[section_name]["files"][arch] = filename
 
     with open(output_file, "w") as f:
         for section, data in grouped_assets.items():
             f.write(f"[{section}]\n")
             f.write(f"type={data['type']}\n")
             
-            for arch, fname in data['files'].items():
+            for arch, file_data in data['files'].items():
                 if arch == "default":
-                    f.write(f"filename={fname}\n")
+                    f.write(f"filename={file_data['filename']}\n")
                 else:
-                    f.write(f"filename_{arch}={fname}\n")
+                    f.write(f"filename_{arch}={file_data['filename']}\n")
                     
             f.write(f"base_url={raw_base}\n")
             f.write(f"base_url_mirror={mirror_base}\n")
             
-            for arch, fname in data['files'].items():
-                clean_path = f"_static/firmware/{fname}"
-                full_link = f"{raw_base}{clean_path}"
+            for arch, file_data in data['files'].items():
+                full_link = f"{raw_base}{file_data['rel_path']}"
                 
                 if arch == "default":
                     f.write(f"download_url={full_link}\n")
